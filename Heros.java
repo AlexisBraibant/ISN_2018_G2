048cbb467815a01258281a86417a142500b22ff2
Benoit Chauviere <benoit@pc-benito.home>
2018-10-10 11:16:04 +0200

    ajout du héros

---
+++ Heros.java
@@ -0,0 +1,46 @@
+public class Heros
+{
+	int pointDeVie;
+	int degat;
+
+	boolean enVie;
+
+	int coorX;
+	int coorY;
+
+	public Heros()
+	{
+		this.pointDeVie = 3;
+		this.degat = 1;
+		this.enVie = true;
+		this.coorX = 0;
+		this.coorY = 0;
+	}
+
+	public void deplacement(char direction)
+	{
+		if (deplacementPossible(direction))
+		{
+			switch (direction)
+			{
+				case 'z':
+					coorY += 1;
+					break;
+				case 's':
+					coorY += -1;
+					break;
+				case 'q':
+					coorX += -1;
+					break;
+				case 'd':
+					coorX += +1;
+					break;
+			}
+		}
+	}
+
+	private boolean deplacementPossible(char direction)
+	{
+		return true;
+	}
+}
