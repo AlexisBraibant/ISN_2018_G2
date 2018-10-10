034e6fee91b7e60e6626b2f5751dec7c67fb1cf5
AlexisBraibant <43601605+AlexisBraibant@users.noreply.github.com>
2018-10-10 12:14:07 +0200

    debut prog LabyFichier

--- src/projetLabyrinthe/LabyFichier.java
+++ src/projetLabyrinthe/LabyFichier.java
@@ -4,10 +4,35 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class LabyFichier
 {
 	public static int compteur = 0;
+	
+	public static String[] affichageFichier(String nomFichier) throws IOException {
+		
+		String[] raw = lireFichier(nomFichier).split("");
+		String[] res = {};
+		for (int i = 0; i < raw.length; i++)
+		{
+			System.out.println(i);
+			System.out.println(raw[i]);
+			if (raw[i].equals("1"))
+			{
+				res[i]="#";
+			} else if (raw[i].equals("0")) {
+				res[i]=" ";
+			} else if (raw[i].equals("2")) {
+				res[i]="E";
+			}
+		}
+		System.out.println(Arrays.toString(res));
+		return res;
+	}
+	
+	
+	
 	public static String lireFichier(String nomFichier) throws IOException
 	{
 		File f = new File(nomFichier);
