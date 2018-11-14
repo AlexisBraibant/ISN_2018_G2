a38a9b984b0a049fd3a688ae433d2a64b9a6456b
chauvier3u <benoitchauviere@hotmail.fr>
2018-11-14 11:25:30 +0100

    zombie et monstre

--- src/projetLabyrinthe/Zombie.java
+++ src/projetLabyrinthe/Zombie.java
@@ -4,5 +4,21 @@ package projetLabyrinthe;
 
 public class Zombie extends Monstre
 {
+	Zombie(LabyFichier Lab)
+	{
+		super(1, 1, 2, 2, Lab);
+		this.tilePerso = 'Z';
+	}
 
+	public char deplacementAleatoire(LabyFichier Lab)
+	{
+		char dir_al;
+		boolean deplPoss;
+		do
+		{
+			dir_al = this.directionAleatoire();
+			deplPoss = this.deplacementPossible(dir_al, Lab);
+		} while (!deplPoss);
+		return dir_al;
+	}
 }
